53f32a3d3c8c22c98e9fbc7201c656bfd151c071
growknows <131237080+growknows@users.noreply.github.com>
2023-04-26 11:19:47 +0800

    Update baidu.js

--- uni-app-sdk/middle/baidu.js
+++ uni-app-sdk/middle/baidu.js
@@ -1,24 +1,24 @@
 // 原生SDK提供的API
-let sensors = require('../jssdk/baidu.js');
+let zallSDK = require('../jssdk/baidu.js');
 
 // 提供各端一致的公共API
 
 let sa = {
 	// 提供扩展性
-	instance: sensors,
+	instance: zallSDK,
 	// 提供初始化和配置参数
-	init: sensors.init.bind(sensors),
+	init: zallSDK.init.bind(zallSDK),
 	setPara: (para) => {
 		para = para || {};
 		let defaultValue = {
 			autoTrack: false
 		};
 		Object.assign(defaultValue, para);
-		sensors.setPara.call(sensors, defaultValue);
+		zallSDK.setPara.call(zallSDK, defaultValue);
 	},
 	// 各端通用的常用API
-	getDistinctID: sensors.store.getDistinctId.bind(sensors.store),
-	register: sensors.registerApp.bind(sensors),
+	getDistinctID: zallSDK.store.getDistinctId.bind(zallSDK.store),
+	register: zallSDK.registerApp.bind(zallSDK),
 	clearRegister: () => {
 		console.log('web 中不支持此方法');
 	}
