df9ad18147b547cee8b4f7358d3a6d86d94b3a64
growknows <131237080+growknows@users.noreply.github.com>
2023-04-26 11:20:07 +0800

    Update app.js

--- uni-app-sdk/middle/app.js
+++ uni-app-sdk/middle/app.js
@@ -1,9 +1,9 @@
 // 原生SDK提供的API,直接实现app对应的所有js的api，在这里再做一次bridge的api封装
-let sensors = uni.requireNativePlugin('Sensorsdata-UniPlugin-App');
+let zallSDK = uni.requireNativePlugin('zallSDKdata-UniPlugin-App');
 
 let sa = {
 	// 提供扩展性
-	instance: sensors,
+	instance: zallSDK,
 
 	setPara: (para) => {
 		if (typeof para !== 'object') {
@@ -21,13 +21,13 @@ let sa = {
 		};
 		Object.keys(para).forEach(
 			(key) => {
-				if (key in relation && typeof sensors[relation[key]] === 'function') {
+				if (key in relation && typeof zallSDK[relation[key]] === 'function') {
 					if (key === 'app_data_collect') {
 						if (para[key] === true) {
-							sensors[relation[key]].call(sensors, para[key]);
+							zallSDK[relation[key]].call(zallSDK, para[key]);
 						}
 					} else {
-						sensors[relation[key]].call(sensors, para[key]);
+						zallSDK[relation[key]].call(zallSDK, para[key]);
 					}
 				} else {
 					console.log('setPara 在 App 中不支持设置' + key);
@@ -40,76 +40,76 @@ let sa = {
 	},
 
 	// app专用的方法
-	getAppFlushInterval: sensors.getFlushInterval.bind(sensors),
-	getAppFlushBulkSize: sensors.getFlushBulkSize.bind(sensors),
-	getAppSessionIntervalTime: sensors.getSessionIntervalTime.bind(sensors),
-	trackAppInstall: sensors.trackAppInstall.bind(sensors),
-	appFlush: sensors.flush.bind(sensors),
+	getAppFlushInterval: zallSDK.getFlushInterval.bind(zallSDK),
+	getAppFlushBulkSize: zallSDK.getFlushBulkSize.bind(zallSDK),
+	getAppSessionIntervalTime: zallSDK.getSessionIntervalTime.bind(zallSDK),
+	trackAppInstall: zallSDK.trackAppInstall.bind(zallSDK),
+	appFlush: zallSDK.flush.bind(zallSDK),
 
-	register: sensors.registerSuperProperties.bind(sensors),
-	unRegister: sensors.unregisterSuperProperty.bind(sensors),
-	clearRegister: sensors.clearSuperProperties.bind(sensors),
-	setProfile: sensors.profileSet.bind(sensors),
-	setOnceProfile: sensors.profileSetOnce.bind(sensors),
-	incrementProfile: sensors.profileIncrement.bind(sensors),
-	appendProfile: sensors.profileAppend.bind(sensors),
-	unsetProfile: sensors.profileUnset.bind(sensors),
-	deleteProfile: sensors.profileDelete.bind(sensors),
+	register: zallSDK.registerSuperProperties.bind(zallSDK),
+	unRegister: zallSDK.unregisterSuperProperty.bind(zallSDK),
+	clearRegister: zallSDK.clearSuperProperties.bind(zallSDK),
+	setProfile: zallSDK.profileSet.bind(zallSDK),
+	setOnceProfile: zallSDK.profileSetOnce.bind(zallSDK),
+	incrementProfile: zallSDK.profileIncrement.bind(zallSDK),
+	appendProfile: zallSDK.profileAppend.bind(zallSDK),
+	unsetProfile: zallSDK.profileUnset.bind(zallSDK),
+	deleteProfile: zallSDK.profileDelete.bind(zallSDK),
 
 	//
-	clearTrackTimer: sensors.clearTrackTimer.bind(sensors),
-	resumeTrackScreenOrientation: sensors.resumeTrackScreenOrientation.bind(sensors),
-	stopTrackScreenOrientation: sensors.stopTrackScreenOrientation.bind(sensors),
-	getScreenOrientation: sensors.getScreenOrientation.bind(sensors),
-	getSuperProperties: sensors.getSuperProperties.bind(sensors),
+	clearTrackTimer: zallSDK.clearTrackTimer.bind(zallSDK),
+	resumeTrackScreenOrientation: zallSDK.resumeTrackScreenOrientation.bind(zallSDK),
+	stopTrackScreenOrientation: zallSDK.stopTrackScreenOrientation.bind(zallSDK),
+	getScreenOrientation: zallSDK.getScreenOrientation.bind(zallSDK),
+	getSuperProperties: zallSDK.getSuperProperties.bind(zallSDK),
 	profileUnsetPushId: (pushKey = '') => {
-		sensors.profileUnsetPushId.call(sensors, pushKey)
+		zallSDK.profileUnsetPushId.call(zallSDK, pushKey)
 	},
 	profilePushId: (pushKey = '', pushId = '') => {
-		sensors.profilePushId.call(sensors, pushKey, pushId)
+		zallSDK.profilePushId.call(zallSDK, pushKey, pushId)
 	},
 	enableTrackScreenOrientation: (enable = false) => {
-		sensors.enableTrackScreenOrientation.call(sensors, enable)
+		zallSDK.enableTrackScreenOrientation.call(zallSDK, enable)
 	},
 	trackViewScreen: (url = '', properties = {}) => {
-		sensors.trackViewScreen.call(sensors, url, properties)
+		zallSDK.trackViewScreen.call(zallSDK, url, properties)
 	},
 	trackTimerEnd: (eventName = '', properties = {}) => {
-		sensors.trackTimerEnd.call(sensors, eventName, properties)
+		zallSDK.trackTimerEnd.call(zallSDK, eventName, properties)
 	},
 	trackTimerResume: (eventName = '') => {
-		sensors.trackTimerResume.call(sensors, eventName)
+		zallSDK.trackTimerResume.call(zallSDK, eventName)
 	},
 	trackTimerPause: (eventName = '') => {
-		sensors.trackTimerPause.call(sensors, eventName)
+		zallSDK.trackTimerPause.call(zallSDK, eventName)
 	},
 	trackTimerStart: (eventName = '') => {
-		return sensors.trackTimerStart.call(sensors, eventName)
+		return zallSDK.trackTimerStart.call(zallSDK, eventName)
 	},
 	removeTimer: (eventName = '') => {
-		sensors.removeTimer.call(sensors, eventName)
+		zallSDK.removeTimer.call(zallSDK, eventName)
 	},
 	enableDeepLinkInstallSource: (enable = false) => {
-		sensors.enableDeepLinkInstallSource.call(sensors, enable);
+		zallSDK.enableDeepLinkInstallSource.call(zallSDK, enable);
 	},
 	trackDeepLinkLaunch: (deepLinkUrl = '', oaid = '') => {
-		sensors.trackDeepLinkLaunch.call(sensors, deepLinkUrl, oaid);
+		zallSDK.trackDeepLinkLaunch.call(zallSDK, deepLinkUrl, oaid);
 	},
 	//
 	popupLoadSuccess: (callback) => {
-		sensors.popupLoadSuccess(callback);
+		zallSDK.popupLoadSuccess(callback);
 	},
 	popupClose: (callback) => {
-		sensors.popupClose(callback);
+		zallSDK.popupClose(callback);
 	},
 	popupClick: (callback) => {
-		sensors.popupClick(callback);
+		zallSDK.popupClick(callback);
 	},
 	popupLoadFailed: (callback) => {
-		sensors.popupLoadFailed(callback);
+		zallSDK.popupLoadFailed(callback);
 	},
 	enablePopup: () => {
-		sensors.enablePopup();
+		zallSDK.enablePopup();
 	}
 
 };
