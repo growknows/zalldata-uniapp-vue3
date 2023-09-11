d3a7b75e4d213dd8626737a309fd207c87a85b22
zhou <844205636@qq.com>
2023-09-11 18:02:12 +0800

    Add files via upload

---
+++ sdk-demo/src/components/child.vue
@@ -0,0 +1,56 @@
+<template>
+  <view class="container">
+		<view class="wrapper">
+			<text>哈喽，我是child组件</text>
+		</view>
+		<view class="btn-item">
+			<button
+					type="primary"
+							@tap="onEvent"
+							data-name="我是child"
+							data-content="这是一个任意点击的采集事件"
+			>
+				我是child
+			</button>
+		</view>
+		<mpclick />
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import mpclick from "@/components/mpclick"
+export default defineComponent({
+	components: { mpclick },
+	setup() {
+
+		const onEvent = () => {
+			console.log('child')
+		}
+		return {
+			onEvent
+		}
+	}
+})
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  background: #2c405a;
+	padding: 5% 0;
+	color: #ffffff;
+}
+
+.btn-item {
+  margin: 10px;
+}
+
+.btn-item button {
+  width: 200px;
+}
+</style>
---
+++ sdk-demo/src/components/mpclick.vue
@@ -0,0 +1,49 @@
+<template>
+  <view>
+		<view class="text-btn--inner"	@tap="onEvent(1)"
+				>
+			<view @tap="onEvent(2)">
+				<view @tap="onEvent(3)">
+					<view @tap.stop="onEvent(4)">
+						我是嵌套的事件mpClick
+					</view>
+				</view>
+			</view>
+		</view>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+		const onEvent = (deep) => {
+			console.log('mpClick', deep)
+		}
+		return {
+			onEvent
+		}
+	}
+})
+</script>
+
+<style>
+
+.text-btn--inner {
+  width: 200px;
+	height: 46px;
+	line-height: 46px;
+	border-radius: 5px;
+	cursor: pointer;
+	font-size: 18px;
+	text-align: center;
+	color: #dd524d;
+	background-color: transparent;
+	border: 1px solid #1aad19;
+}
+.text-btn--inner:hover {
+	background-color: #1aad19;
+
+}
+</style>
