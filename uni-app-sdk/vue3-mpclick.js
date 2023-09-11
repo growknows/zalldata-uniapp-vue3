9b8be04ae276330ae6a5ce51d153e7b675073dfd
zhou <844205636@qq.com>
2023-09-11 17:59:43 +0800

    Add files via upload

--- uni-app-sdk/vue3-mpclick.js
+++ uni-app-sdk/vue3-mpclick.js
@@ -38,67 +38,74 @@ export default function get_enableVue3MpClick(sensors) {
     // #endif
 
 
-    let oldCreatePage = platform.createPage;
-    if (oldCreatePage) {
-      platform.createPage = function (MiniProgramPage) {
-        let oldRender = MiniProgramPage.render || MiniProgramPage.setup;
-        const _setup =  function (props, ctx) {
-          let bindingConfig = oldRender.apply(MiniProgramPage, arguments);
-          let _bindingConfig = bindingConfig
-          bindingConfig = function (_ctx, _cache) {
-            let retConfig = _bindingConfig.apply(MiniProgramPage, arguments);
+    function platformProxy (sfcComponent) {
+      let oldRender = sfcComponent.render || sfcComponent.setup;
+      const _setup =  function (props, ctx) {
+        let bindingConfig = oldRender.apply(MiniProgramPage, arguments);
+        let _bindingConfig = bindingConfig
+        bindingConfig = function (_ctx, _cache) {
+          let retConfig = _bindingConfig.apply(MiniProgramPage, arguments);
 
-            let mpInstance = _ctx && _ctx.$scope;
-            if (mpInstance) {
-              for (let i in mpInstance) {
-                if (typeof mpInstance[i] === 'function') {
-                  let oldFn = mpInstance[i];
-                  if (sa.para.autoTrack && sa.para.autoTrack.mpClick) {
-                    mpInstance[i] = function () {
-                      oldFn.apply(mpInstance, arguments);
-                      if(arguments[0] instanceof Object) clickTrack(arguments[0])
-                    }
-                  }
-                }
-              }
-            }
-            return retConfig
-          }
-
-
-          return bindingConfig;
-        }
-        const _render = function (ctx) {
-          var bindingConfig = oldRender.apply(MiniProgramPage, arguments);
-          var mpInstance = ctx && ctx.$scope;
+          let mpInstance = _ctx && _ctx.$scope;
           if (mpInstance) {
-            for (let i in bindingConfig) {
-              var propKey = bindingConfig[i];
-              if (typeof mpInstance[propKey] === 'function') {
-                let oldFn = mpInstance[propKey];
+            for (let i in mpInstance) {
+              if (typeof mpInstance[i] === 'function') {
+                let oldFn = mpInstance[i];
                 if (sa.para.autoTrack && sa.para.autoTrack.mpClick) {
-                  mpInstance[propKey] = function () {
+                  mpInstance[i] = function () {
                     oldFn.apply(mpInstance, arguments);
-                    clickTrack(arguments[0])
+                    if(arguments[0] instanceof Object) clickTrack(arguments[0])
                   }
                 }
               }
             }
           }
-          return bindingConfig;
+          return retConfig
         }
 
 
-        if(MiniProgramPage.render) {
-          MiniProgramPage.render = _render
-        }else if(MiniProgramPage.setup) {
-          MiniProgramPage.setup = _setup
-        }
-
-        oldCreatePage.apply(platform, arguments);
+        return bindingConfig;
       }
+      const _render = function (ctx) {
+        var bindingConfig = oldRender.apply(sfcComponent, arguments);
+        var mpInstance = ctx && ctx.$scope;
+        if (mpInstance) {
+          for (let i in bindingConfig) {
+            var propKey = bindingConfig[i];
+            if (typeof mpInstance[propKey] === 'function') {
+              let oldFn = mpInstance[propKey];
+              if (sa.para.autoTrack && sa.para.autoTrack.mpClick) {
+                mpInstance[propKey] = function () {
+                  oldFn.apply(mpInstance, arguments);
+                  clickTrack(arguments[0])
+                }
+              }
+            }
+          }
+        }
+        return bindingConfig;
+      }
+
+      if(sfcComponent.render) {
+        sfcComponent.render = _render
+      }else if(sfcComponent.setup) {
+        sfcComponent.setup = _setup
+      }
+
+      oldCreatePage.apply(platform, arguments);
     }
 
+    let oldCreatePage = platform.createPage;
+    if (oldCreatePage) {
+      platform.createPage = platformProxy
+    }
+
+    let oldCreateComponent = platform.createComponent;
+    if (oldCreateComponent) {
+      platform.createComponent = platformProxy
+    }
+
+    
     function clickTrack(events) {
       var _ = sa._;
       var prop = {};
